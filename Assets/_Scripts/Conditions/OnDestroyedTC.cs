d3aa7cf7ae1749ab14fdd3c281e2274748cfba01
Adrian Kellner <54819634+Priescik@users.noreply.github.com>
2025-09-30 09:42:01 +0200

    Create OnDestroyedTC.cs

---
+++ Assets/_Scripts/Conditions/OnDestroyedTC.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System;
+
+public class OnDestroyedTC : TriggerCondition
+{
+    public override void SubscribeCondition(Action<GameAction> reaction)
+    {
+        ActionSystem.SubscribeReaction<DestroyEntityGA>(reaction, ReactionTiming);
+    }
+    public override void UnsubscribeCondition(Action<GameAction> reaction)
+    {
+        ActionSystem.UnsubscribeReaction<DestroyEntityGA>(reaction, ReactionTiming);
+    }
+    public override bool SubConditionIsMet(EntityView skillOwnerEntity, GameAction gameAction)
+    {
+        // if target is this
+        if (gameAction is DestroyEntityGA destroyEntityGA 
+            && destroyEntityGA.Targets.Contains(skillOwnerEntity))
+        {
+            return true;
+        }
+        return false;
+        
+    }
+}
